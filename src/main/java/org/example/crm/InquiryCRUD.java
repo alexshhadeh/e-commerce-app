f2c7b2bbfa77e0fb4f18343805c28ebbc7264ebe
alexshhadeh <alexsh2001@hotmail.com>
2022-04-25 11:57:32 +0200

    some changes

---
+++ src/main/java/org/example/crm/InquiryCRUD.java
@@ -0,0 +1,8 @@
+package org.example.crm;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface InquiryCRUD
+        extends JpaRepository<Inquiry, Long> {
+}
